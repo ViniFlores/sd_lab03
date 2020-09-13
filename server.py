6bae77ea756e454acfdaa1aded135a7627d47fab
ViniFlores <viniciusflores93@gmail.com>
2020-09-13 15:32:09 -0300

    final commit

--- server.py
+++ server.py
@@ -1,5 +1,40 @@
 import socket
+import select
+import sys
+import threading
 from collections import Counter
+
+HOST = '127.0.0.1'
+PORT = 65432        
+
+def inicia_servidor():
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.bind((HOST,PORT))
+  sock.listen(5)
+  return sock
+
+def aceita_conexao(sock):
+  conn, endr = sock.accept()
+  return conn, endr
+
+def atende_requisicoes(conn, endr):
+  while True:
+    data = conn.recv(4096)
+    if not data:
+        break
+
+    lista_de_arquivos = eval(data.decode('utf-8'))
+
+    resultado = {}
+
+    for arquivo in lista_de_arquivos:
+      file = open("arquivos/" + arquivo, "r")
+      print('Abrindo arquivo ' + arquivo + '')
+      resultado[arquivo] = conta_palavras(lista_palavras(file.readlines()))
+
+    conn.sendall((str(resultado)).encode())
+
+  
 
 def lista_palavras(text):
   return text[0].split()
@@ -8,30 +43,23 @@ def conta_palavras(array):
   print('Contando palavras')
   return Counter(array).most_common(10)
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+def main():
+  sock = inicia_servidor()
+  print("Pronto para receber conexões...")
+  entradas = [sock] #select da API do Windows só recebe socket, stdin é um file descriptor
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.bind((HOST, PORT))
-  s.listen()
   while True:
-    print('Esperando conexão...')
-    conn, addr = s.accept()
-    with conn:
-        print('Recebendo conexão')
-        while True:
-            data = conn.recv(4096)
-            if not data:
-                break
+    r, w, e = select.select(entradas, [], [])
+    if sys.stdin.isatty():
+      for line in sys.stdin:
+        print(line)
 
-            lista_de_arquivos = eval(data.decode('utf-8'))
+    for pronto in r:
+      if pronto == sock:
+        conn, endr = aceita_conexao(sock)
+        print("Conectado com : ", endr)
+        thread_atendente = threading.Thread(target=atende_requisicoes, args=(conn, endr))
+        thread_atendente.start()
 
-            resultado = {}
 
-            for arquivo in lista_de_arquivos:
-              file = open("arquivos/" + arquivo, "r")
-              print('Abrindo arquivo ' + arquivo + '')
-              resultado[arquivo] = conta_palavras(lista_palavras(file.readlines()))
-
-            conn.sendall((str(resultado)).encode())
-
+main()
